7b09ae7619b83b21dffc77babd77a0f369757403
Maciej Zaweracz <maciekm1pro@gmail.com>
2023-07-15 21:50:15 +0100

    Added Player animation and decoupled input

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private PlayerInput playerInput;
+
+    private bool isWalking;
     private void Update()
     {
-        Vector2 inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        Vector2 inputVector = playerInput.GetMovementVectorNormalized();
         Vector3 movementVector = new Vector3(inputVector.x, 0, inputVector.y);
 
         transform.position += movementVector * Time.deltaTime * moveSpeed;
+
+        isWalking = movementVector != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, movementVector, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
